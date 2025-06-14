f884ab354166ce210f28a0f50c3d6886aa4a4492
JesselZapanta <jesselzapanta9@gmail.com>
2025-06-14 12:32:17 +0800

    first commit

---
+++ resources/js/layouts/app-layout.jsx
@@ -0,0 +1,9 @@
+
+import  AppLayoutTemplate  from '@/layouts/app/app-sidebar-layout';
+// import  AppLayoutTemplate  from '@/layouts/app/app-header-layout';
+
+export default ({ children, breadcrumbs, ...props }) => (
+    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
+        {children}
+    </AppLayoutTemplate>
+);
---
+++ resources/js/layouts/auth-layout.jsx
@@ -0,0 +1,11 @@
+// import AuthLayoutTemplate from '@/layouts/auth/auth-simple-layout';
+import AuthLayoutTemplate from '@/layouts/auth/auth-split-layout'; 
+// import AuthLayoutTemplate from '@/layouts/auth/auth-card-layout'; 
+
+export default function AuthLayout({ children, title, description, ...props }) {
+    return (
+        <AuthLayoutTemplate title={title} description={description} {...props}>
+            {children}
+        </AuthLayoutTemplate>
+    );
+}
